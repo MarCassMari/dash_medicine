4a00847eba7334e936cdb48b0dc41fe3280b9031
MarCassMari <marcassmachado32@gmail.com>
2026-02-15 14:21:54 -0300

    feat:buscando-dados-usuariologado

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,23 @@
-const dashboardPage = () =>{
+"use client"
 
-     return(<div>
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-        GRÁFICOS
+const DashboardPage = () =>{
 
-     </div>);
+   const session = authClient.useSession();
+  
+     return(
+     
+     <div>
+    <h1>Dashboard</h1>
+     <h1>{session?.data?.user?.email}</h1>
+     <h1>{session?.data?.user?.name}</h1>
 
-}
+   <Button onClick={()=>authClient.signOut()}>Sair</Button>
+     </div>
+   );
+
+};
+
+export default DashboardPage;
