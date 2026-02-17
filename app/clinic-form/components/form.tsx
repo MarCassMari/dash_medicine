3afa600e9c3a0624fbc043ed37145ea0cb67c302
MarCassMari <marcassmachado32@gmail.com>
2026-02-17 17:12:55 -0300

    feat:onSubmit formulario

--- app/clinic-form/components/form.tsx
+++ app/clinic-form/components/form.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { createClinic } from "@/actions/create-clinic";
 import { Button } from "@/components/ui/button";
 import { DialogFooter } from "@/components/ui/dialog";
 import { Form, FormControl, FormField, FormItem, FormLabel } from "@/components/ui/form"
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Loader2 } from "lucide-react";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 import { z } from "zod";
 
@@ -13,9 +16,18 @@ const clinicFormSchema = z.object({
     name: z.string().trim().min(1,{message: "O nome obrigatório."})
     });
 
-const onSubmit = (data: z.infer<typeof clinicFormSchema>) => {
-    console.log(data);
-  }
+const onSubmit = async (data: z.infer<typeof clinicFormSchema>) => {
+try{
+
+await createClinic(data.name);
+toast.success("Clínica criada com sucesso");
+
+}catch(error){
+  console.error(error);
+  toast.error("Erro ao criar clínica");
+
+}
+}
 
   
 export const ClinicForm = () =>{
@@ -43,7 +55,9 @@ return(
 </Form>
 
 <DialogFooter>
-  <Button type="submit">Salvar</Button>
+  <Button type="submit" disabled={form.formState.isSubmitting}>{form.formState.isSubmitting &&(
+    <Loader2 className="w-4 h-4 animate-spin" />
+  )} Criar Clínica</Button>
 </DialogFooter>
     </div>
 )
