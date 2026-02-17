1371e18bda11d158477af6cd4f99a3a5e1a07003
MarCassMari <marcassmachado32@gmail.com>
2026-02-17 14:59:04 -0300

    feat:dialog-component

--- app/clinic-form/page.tsx
+++ app/clinic-form/page.tsx
@@ -1,7 +1,54 @@
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+  import { Input } from "@/components/ui/input"
+  import { Label } from "@/components/ui/label"
+  import { Field, FieldGroup } from "@/components/ui/field"
+
 const ClinicFormPage = () =>{
     return(
         <div>
             <h1>Formulário da Clínica</h1>
+    <Dialog open >
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-sm">
+          <DialogHeader>
+            <DialogTitle>Adicionar Clinica</DialogTitle>
+            <DialogDescription>
+              Adicione uma nova clínica para o seu usuário. 
+            </DialogDescription>
+          </DialogHeader>
+          <FieldGroup>
+            <Field>
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </Field>
+            <Field>
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </Field>
+          </FieldGroup>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
+    </Dialog>
+
         </div>
     )
 }
